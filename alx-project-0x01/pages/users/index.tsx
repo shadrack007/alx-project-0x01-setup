bc16e185c1294c484b53ff3b661e014d02e4724a
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-05-29 17:18:23 +0300

    implemeted user page

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,18 +1,57 @@
-const Users: React.FC = () => {
+import UserCard from "@/components/common/UserCard";
+import Header from "@/components/layout/Header";
+import { UserPageProps, UserProps } from "@/interfaces";
+
+const Users: React.FC<UserPageProps> = ({ users }) => {
+  console.log("users", users);
+
   return (
-    <main>
-      <h1>users home page</h1>
-    </main>
+    <div className="flex flex-col h-screen">
+      <Header />
+
+      <main className="p-4">
+        <div className="grid grid-cols-3 gap-2">
+          {users.map(
+            (
+              {
+                name,
+                username,
+                email,
+
+                company,
+                id,
+                phone,
+                website,
+                address,
+              }: UserProps,
+              key: number
+            ) => (
+              <UserCard
+                name={name}
+                username={username}
+                email={email}
+                address={address}
+                company={company}
+                id={id}
+                phone={phone}
+                website={website}
+                key={key}
+              />
+            )
+          )}
+        </div>
+      </main>
+    </div>
   );
 };
 
 export async function getStaticProps() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const posts = await response.json();
+  const users = await response.json();
 
   return {
     props: {
-      posts,
+      users,
     },
   };
 }
