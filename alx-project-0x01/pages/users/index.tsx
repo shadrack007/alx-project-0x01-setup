b4389c1cbaa7d2ea983ccf968e493ac9b808827f
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-05-29 15:19:32 +0300

    implemented user page

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -6,4 +6,15 @@ const Users: React.FC = () => {
   );
 };
 
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const posts = await response.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
 export default Users;
