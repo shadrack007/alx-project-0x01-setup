4b42c5d7d61b1fa9207e170c3b4678cb1e1e557a
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-05-29 15:04:31 +0300

    defined post card

--- alx-project-0x01/components/common/PostCard.tsx
+++ alx-project-0x01/components/common/PostCard.tsx
@@ -1,7 +1,18 @@
-const PostCard: React.FC = () => {
+import { PostProps } from "@/interfaces";
+
+const PostCard: React.FC<PostProps> = ({ id, userId, title, body }) => {
   return (
     <div>
-      <p>post card</p>
+      <div>
+        <h2>{title}</h2>
+      </div>
+
+      <p>{body}</p>
+
+      <div>
+        <span>User ID: {userId}</span>
+        <span>Post ID: {id}</span>
+      </div>
     </div>
   );
 };
