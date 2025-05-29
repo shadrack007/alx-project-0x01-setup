6fc2735012dd40c7a8422ba098a259a4eb3b1b88
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-05-29 16:18:24 +0300

    fix: props to Post

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,17 +1,19 @@
 import PostCard from "@/components/common/PostCard";
 import Header from "@/components/layout/Header";
-import { PostProps } from "@/interfaces";
+import { PostPageProps, PostProps } from "@/interfaces";
 
-const Post: React.FC<PostProps[]> = ({ posts }) => {
+const Post: React.FC<PostPageProps> = ({ posts }) => {
   console.log(posts);
   return (
-    <div>
+    <div className="flex flex-col h-screen">
       <Header />
 
       <main className="p-4">
-        <div>
-          <h1>Post Content</h1>
-          <button>Add Post</button>
+        <div className="flex justify-between">
+          <h1 className="text-2xl font-semibold">Post Content</h1>
+          <button className="bg-blue-700 px-4 py-2 rounded-full text-white">
+            Add Post
+          </button>
         </div>
 
         <div className="grid grid-cols-3 gap-2">
